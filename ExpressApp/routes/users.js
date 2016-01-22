fe8e5fd4b6daee5513bbec6c843735eed07179c9
kkdubey1989@gmail.com <kkdubey1989@gmail.com>
2016-01-22 18:47:46 +0530

    my sql chnages

--- ExpressApp/routes/users.js
+++ ExpressApp/routes/users.js
@@ -1,9 +1,29 @@
 ﻿var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database : "test",
+    port: "3306"
+});
 
 /* GET users listing. */
 router.get('/', function (req, res) {
     res.send('respond with a resource');
+});
+router.get('/test', function (req, res) {
+    //res.send('respond with a resource with new url');
+    con.connect();
+    con.query('SELECT * from  person', function (err, rows, fields) {
+        if (err) console.log('The error is : ', err);
+        res.send(rows);
+    });
+    
+    con.end();
 });
 
 module.exports = router;
